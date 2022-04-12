701f27aa69e539913973ef6da9e0fa40c3fb59f1
aalvarado78 <53951526+aalvarado78@users.noreply.github.com>
2022-04-12 12:33:08 -0500

    Add files via upload

---
+++ webapp/src/components/About.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="text-center">
+		<p class="h1">
+			<img src="../assets/Awesome-black.png" style="object-fit:contain; width:512px; height:auto"/>
+			<img src="../assets/NAMJAC.png" style="object-fit:contain; width:512px; height:auto"/>
+		</p>
+        <p class="h1">
+            Created by NAMJAC Development for internal use at Awesome tires and mechanic
+        </p>
+		<br/>
+		<p>
+			<router-link class="display-6 text-decoration-none text-secondary p-3" to="/about/manuals">Manual</router-link>
+		</p>
+    </div>
+</template>
